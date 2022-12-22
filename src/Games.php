d3602c787a1ecd8593509f6cd831863faefd63f2
LexAeterna731 <Delirium731@yandex.ru>
2022-12-22 21:43:19 +0700

    Add Games.php

---
+++ src/Games.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Brain\Games\Games;
+
+use function cli\line;
+use function cli\prompt;
+
+function startGame($welcomeMessage, $questionAnswerPairs)
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt("May I have your name?");
+    line("Hello, %s!", $name);
+    line($welcomeMessage);
+
+    for ($i = 0, $questionsCount = count($questionAnswerPairs); $i < $questionsCount; $i += 1) {
+        $question = $questionAnswerPairs[$i][0];
+        $answer = $questionAnswerPairs[$i][1];
+        line("Question: %s", $question);
+        $userAnswer = prompt("Your answer");
+        if ($userAnswer !== $answer) {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $userAnswer, $answer);
+            line("Let's try again, %s!", $name);
+
+            break;
+        } else {
+            line("Correct!");
+        }
+    }
+
+    if ($i === $questionsCount) {
+        line("Congratulations, %s!", $name);
+    }
+}
