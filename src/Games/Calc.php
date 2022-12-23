afb7c1c6d96afaf9c1a6c0356672bc4b6519a9f9
LexAeterna731 <Delirium731@yandex.ru>
2022-12-23 13:06:40 +0700

    Add Games/Calc.php

---
+++ src/Games/Calc.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Brain\Games\Games\Calc;
+
+use function Brain\Games\Engine\startGame;
+
+function startCalc()
+{
+    $welcomeMessage = "What is the result of the expression?";
+
+    $questionAnswerPairs = [];
+    for ($i = 0, $numberOfQuestions = 3; $i < $numberOfQuestions; $i += 1) {
+        $firstOperand = rand(1, 1000);
+        $secondOperand = rand(1, 1000);
+        $operationId = rand(1, 3);
+        switch ($operationId) {
+            case 1:
+                $question = "{$firstOperand} + {$secondOperand}";
+                $answer = $firstOperand + $secondOperand;
+                break;
+            case 2:
+                $question = "{$firstOperand} - {$secondOperand}";
+                $answer = $firstOperand - $secondOperand;
+                break;
+            case 3:
+                $question = "{$firstOperand} * {$secondOperand}";
+                $answer = $firstOperand * $secondOperand;
+                break;
+        }
+        $questionAnswerPairs[] = [$question, $answer];
+    }
+
+    startGame($welcomeMessage, $questionAnswerPairs);
+}
